43d15ab28f0503b1cc7f921b86933fbd4486cb68
IulianaIagodka <iuliana.iagodka@gmail.com>
2020-02-04 13:57:29 +0200

    update scripts

--- echo_bot.py
+++ echo_bot.py
@@ -1,8 +1,18 @@
 import json
 import requests
+from bs4 import BeautifulSoup
 
-TOKEN = "put_your_token"
+TOKEN = "1015415531:AAGLP49ZJjWPXvPkL-Qh6ljyBnDYvWp0Ddo"
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+url_news = "https://towardsdatascience.com/"
+
+
+r1 = requests.get(url_news)
+coverpage = r1.content
+soup1 = BeautifulSoup(coverpage, "html5lib")
+coverpage_news = soup1.find_all("h3", class_="u-contentSansBold u-lineHeightTightest u-xs-fontSize24 u-paddingBottom2 u-paddingTop5 u-fontSize32")
+first_new = coverpage_news[0].get_text()
+#print(first_new)
 
 
 def get_url(url):
@@ -35,7 +45,8 @@ def send_message(text, chat_id):
     url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
     get_url(url)
 
+
 text, chat = get_last_chat_id_and_text(get_updates())
 #send_message(text, chat)
-send_message(f"{text} ?", chat)
-print (text)
+send_message(f"{first_new}", chat)
+#print (text)
